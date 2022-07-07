78f21c7e7b2119ecea6da46d38e872b3cf3af3f0
WoonThye <20018265@myrp.edu.sg>
2022-07-07 12:02:01 +0800

    edits made

--- web/test.php
+++ web/test.php
@@ -1,10 +1,13 @@
-<!-- fix : <html> element does not have a [lang] attribute -->
+<!DOCTYPE html>
 <html lang="en">
-<html>
+    
 <head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
 </head>
 <body>
-  Hello World!
+    Hello world test
 </body>
 </html>
- 
